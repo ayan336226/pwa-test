dcbc460e91d8cf4674eec39ffe6c3c4a4591c81a
ayan336226 <muhammadayan336226@gmail.com>
2026-01-04 13:08:09 +0500

    Apka message yahan

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,7 @@
-const CACHE_NAME = "pwa-final-cache-v1";
+const CACHE_NAME = "pwa-final-cache-v3";
 
 const FILES_TO_CACHE = [
+  "./",
   "index.html",
   "style.css",
   "manifest.json",
@@ -9,16 +10,33 @@ const FILES_TO_CACHE = [
   "icons/icon-512.png"
 ];
 
+// Install
 self.addEventListener("install", event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
   );
+  self.skipWaiting(); // force activate new SW
 });
 
+// Activate – old cache delete
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+// Fetch
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(
-      response => response || fetch(event.request)
-    )
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
   );
 });
